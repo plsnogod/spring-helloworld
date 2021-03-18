c521d1f5f3e4839dc3d736f492bab9445d328cf3
plsnogod <plsnogood@mail.ru>
2021-03-18 12:36:50 +0300

    Spring

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,26 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+//        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+
+
+        ApplicationContext applicationContext1 =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        Cat bean2 = (Cat) applicationContext.getBean("cat");
+ //       System.out.println(bean2.getMessage());
+        ApplicationContext applicationContext2 =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+ //       System.out.println(bean3.getMessage());
+
+//        System.out.println(bean.getMessage());
+//        System.out.println(bean.getMessage());
+//        System.out.println(bean2.getMessage());
+//        System.out.println(bean2.getMessage());
+        System.out.println(bean==bean);
+        System.out.println(bean2==bean3);
     }
 }
